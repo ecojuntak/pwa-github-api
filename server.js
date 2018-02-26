71e5372eb16f7b54c0a8d133b0ba5205197a60a3
Eko Simanjuntak <if415009@students.del.ac.id>
2018-02-27 01:12:28 +0700

    fixing server.js

--- server.js
+++ server.js
@@ -3,7 +3,10 @@ var path = require('path');
 var serveStatic = require('serve-static');
 
 app = express();
-
 app.use('/', serveStatic(__dirname + "/dist"));
+app.get('*', function (req, res) {
+    res.sendFile(__dirname + '/dist/index.html')
+})
+
 var port = process.env.PORT || 5000;
 app.listen(port);
